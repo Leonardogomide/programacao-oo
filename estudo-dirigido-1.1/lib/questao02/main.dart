a0926e4c5e92e233a36a266be7948c0caf2224ff
Leo <158283796+Leonardogomide@users.noreply.github.com>
2026-03-30 13:33:42 -0300

    Update main.dart

--- estudo-dirigido-1.1/lib/questao02/main.dart
+++ estudo-dirigido-1.1/lib/questao02/main.dart
@@ -14,11 +14,17 @@ Restrições:
 import 'relogio.dart';
 
 void main(){
-  Relogio relogio = new Relogio(8,15,45);
-  Relogio relogio2 = new Relogio(14,30,0);
-  Relogio relogio3 = new Relogio(0,0,0);
+  // Instancia o primeiro relógio com 8 horas, 15 minutos e 45 segundos
+  Relogio relogio = new Relogio(8, 15, 45);
+  // Instancia o segundo relógio com 14 horas, 30 minutos e 0 segundos
+  Relogio relogio2 = new Relogio(14, 30, 0);
+  // Instancia o terceiro relógio com 0 horas, 0 minutos e 0 segundos
+  Relogio relogio3 = new Relogio(0, 0, 0);
 
+  // Exibe o horário do primeiro relógio
   relogio.exibirHorario();
+  // Exibe o horário do segundo relógio
   relogio2.exibirHorario();
+  // Exibe o horário do terceiro relógio
   relogio3.exibirHorario();
 }
